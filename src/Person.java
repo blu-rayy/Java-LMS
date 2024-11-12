94cb4af5ea8524412cfb761e0f22424cb0f77a3c
Ianne <mariannesantos174@gmail.com>
2024-11-12 22:05:38 +0800

    Parent Person class with attributes and methods.

---
+++ src/Person.java
@@ -0,0 +1,26 @@
+public class Person {
+    private String name;
+    private String id;
+
+    public Person(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public boolean borrowBook(Book book) {
+        return false;
+    }
+
+    public void returnBook(Book book) {
+    }
+}
+
+
