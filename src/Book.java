4a23d493f6c71557ac47c21c745b287aca0b2f1b
Ianne <mariannesantos174@gmail.com>
2024-11-12 21:53:04 +0800

    Book class added with check attributes and methods.

---
+++ src/Book.java
@@ -0,0 +1,67 @@
+public class Book {
+    private String title;
+    private String author;
+    private String ISBN;
+    private String publicationDate;
+    private int availableCopies;
+
+    public Book(String title, String author, String ISBN, String publicationDate, int availableCopies) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.publicationDate = publicationDate;
+        this.availableCopies = availableCopies;
+}
+
+public String getTitle() {
+    return title;
+}
+
+public void setTitle(String title) {
+    this.title = title;
+}
+
+public String getAuthor() {
+    return author;
+}
+
+public void setAuthor(String author) {
+    this.author = author;
+}
+
+public String getISBN() {
+    return ISBN;
+}
+
+public void setISBN(String ISBN) {
+    this.ISBN = ISBN;
+}
+
+public String getPublicationDate() {
+    return publicationDate;
+}
+
+public void setPublicationDate(String publicationDate) {
+    this.publicationDate = publicationDate;
+}
+
+public int getAvailableCopies() {
+    return availableCopies;
+}
+
+public void setAvailableCopies(int availableCopies) {
+    this.availableCopies = availableCopies;
+}
+
+public boolean borrowBook() {
+    if (availableCopies > 0) {
+        availableCopies--;
+        return true;
+    }
+    return false;
+}
+
+public void returnBook() {
+    availableCopies++;
+}
+}
