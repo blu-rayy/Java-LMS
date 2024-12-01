5978b4929c8406077d84ef682ef2250e62db1360
Ianne <mariannesantos174@gmail.com>
2024-12-01 14:56:57 +0800

    syncing changes

--- src/backend/Main.java
+++ src/backend/Main.java
@@ -8,10 +8,9 @@ public class Main {
         // NOTE: when creating a Librarian Account, the Verification Password is: adminLibrary.147
 
         LibraryDatabase.createTables();
-
-        //LibraryDatabase.populateBooks();
-        //LibraryDatabase.populateAuthors();
-        //LibraryDatabase.populateMembers();
+        LibraryDatabase.populateBooks();
+        LibraryDatabase.populateAuthors();
+        LibraryDatabase.populateMembers();
         
         ANPLMSGUI gui = new ANPLMSGUI();
         gui.setVisible(true);
